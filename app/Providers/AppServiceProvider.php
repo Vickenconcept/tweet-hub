a81ee7f0cf93c6e866380b6fed56b3226670dc56
William Victor <vicken408@gmail.com>
2026-09-02 20:35:02 +0100

    update on whatsapp

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,7 +2,9 @@
 
 namespace App\Providers;
 
+use App\Livewire\WhatsAppSettingsComponent;
 use Illuminate\Support\ServiceProvider;
+use Livewire\Livewire;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +21,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        // Livewire kebab-cases WhatsAppSettingsComponent as "whats-app-settings-component",
+        // but views use "whatsapp-settings-component".
+        Livewire::component('whatsapp-settings-component', WhatsAppSettingsComponent::class);
     }
 }
